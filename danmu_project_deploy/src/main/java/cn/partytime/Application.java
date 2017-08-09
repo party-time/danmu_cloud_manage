d1133396aea7810ffc8362423bdeb884d8b0369d
soaryang <longfei19880>
2017-08-09 16:53:57 +0800

    弹幕cloud管理模块

--- danmu_project_deploy/src/main/java/cn/partytime/Application.java
+++ danmu_project_deploy/src/main/java/cn/partytime/Application.java
@@ -5,10 +5,15 @@ package cn.partytime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("cn.partytime")
+@EnableDiscoveryClient
+@EnableFeignClients
+@RefreshScope
 public class Application {
 
 	public static void main(String[] args) throws Exception {
